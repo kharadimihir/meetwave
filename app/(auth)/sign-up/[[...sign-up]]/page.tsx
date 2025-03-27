255c50061935aa916aee1c54c000e19d4b047408
kharadimihir <kharadimihir739@gmail.com>
2025-03-27 14:27:18 +0530

    MeetingId

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,8 +3,8 @@ import { SignUp } from '@clerk/nextjs'
 const SignUpPage = () => {
   
   return (
-    <main className='flex h-screen w-full items-center justify-center'>
-      <SignUp />
+    <main className='flex flex-row h-screen w-full items-center justify-center'>
+       <SignUp />
     </main>
   )
 }
